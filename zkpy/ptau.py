d56a461bba88ef8724caafa21dd51a0df303b87e
sahilmahendrakar <sahilm020@gmail.com>
2023-02-19 20:49:57 -0500

    Implemented function to prepare for phase 2 in ptau

--- zkpy/ptau.py
+++ zkpy/ptau.py
@@ -7,8 +7,11 @@ import uuid
 
 PUBLIC_ENTROPY="0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
 
+def gen_ptau_file():
+    return str(uuid.uuid4())+".ptau"
+
 class PTau:
-    def __init__(self, ptau_file=str(uuid.uuid4())+".ptau"):
+    def __init__(self, ptau_file=gen_ptau_file()):
         self.ptau_file = ptau_file
 
     # TODO: Check return code of process
@@ -19,7 +22,7 @@ class PTau:
         proc = subprocess.run(['snarkjs', 'powersoftau', 'new', curve, constraints, self.ptau_file, "-v"], capture_output=True)
         
     # Contributes randomness (entropy) to power of tau ceremony
-    def contribute(self, name="", entropy="", output_file=str(uuid.uuid4())+".ptau"):
+    def contribute(self, name="", entropy="", output_file=gen_ptau_file()):
         # If no random text is supplied, generate 100 random characters
         if entropy == "":
             entropy = ''.join(random.choices(string.ascii_lowercase, k=100))
@@ -31,13 +34,17 @@ class PTau:
     # TODO: Handle import / export contributions from 3rd party software
 
     # Finalizes phase 1 of the power of tau ceremony
-    def beacon(self, output_file=str(uuid.uuid4())+".ptau", public_entropy=PUBLIC_ENTROPY, iter=10):
+    def beacon(self, output_file=gen_ptau_file(), public_entropy=PUBLIC_ENTROPY, iter=10):
         proc = subprocess.run(["snarkjs", "powersoftau", "beacon", self.ptau_file, output_file, public_entropy, str(iter)], capture_output=True)
+        self.ptau_file = output_file
+
+    def prep_phase2(self, output_file=gen_ptau_file()):
+        proc = subprocess.run(["snarkjs", "powersoftau", "prepare", "phase2", self.ptau_file, output_file, "-v"], capture_output=True)
         self.ptau_file = output_file
 
     def verify(self):
         proc = subprocess.run(["snarkjs", "powersoftau", "verify", self.ptau_file], capture_output=True)
-        print(proc.stdout.decode('utf-8'))
+        print(proc.stdout.decode('utf-8'))    
 
     # TODO: Add way to cleanup files
 
@@ -49,5 +56,7 @@ print("Contribute")
 ptau.contribute()
 print("Beacon")
 ptau.beacon()
+print("Phase2")
+ptau.prep_phase2()
 print("Verify")
 ptau.verify()
