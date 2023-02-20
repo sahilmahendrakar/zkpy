141e040f69a4841cd2c6e1f25ad7b4f360771912
sahilmahendrakar <sahilm020@gmail.com>
2023-02-19 20:21:51 -0500

    Refactored contribute to include an output file

--- zkpy/ptau.py
+++ zkpy/ptau.py
@@ -12,30 +12,23 @@ class PTau:
     # TODO: Check return code of process
     # Begins a power of tau ceremony, curve is the curve to use 
     # and constraints is the number of constraints raised to the power of 2
-    def start(self, curve='bn128', constraints='14'):
+    def start(self, curve='bn128', constraints='12'):
         # snarkjs powersoftau new bn128 14 pot14_0000.ptau -v
         proc = subprocess.run(['snarkjs', 'powersoftau', 'new', curve, constraints, self.ptau_file, "-v"], capture_output=True)
         
-    def contribute(self, name="", entropy=""):
+    # Contributes randomness (entropy) to power of tau ceremony
+    def contribute(self, name="", entropy="", output_file=str(uuid.uuid4())+".ptau"):
         # If no random text is supplied, generate 100 random characters
         if entropy == "":
             entropy = ''.join(random.choices(string.ascii_lowercase, k=100))
         if entropy[-1] != "\n":
             entropy += "\n"
-        new_ptau_file = str(uuid.uuid4())+".ptau"
-        proc = subprocess.run(["snarkjs", "powersoftau", "contribute", self.ptau_file, new_ptau_file, f'--name="{name}"', "-v", f'-e={entropy}'], capture_output=True,)
-        self.ptau_file = new_ptau_file
+        proc = subprocess.run(["snarkjs", "powersoftau", "contribute", self.ptau_file, output_file, f'--name="{name}"', "-v", f'-e={entropy}'], capture_output=True,)
+        self.ptau_file = output_file
         print(proc.stdout.decode('utf-8'))
 
     # TODO: Handle import / export contributions from 3rd party software
 
-    def test(self):
-        proc = subprocess.run(['snarkjs', '--help'], capture_output=True)
-        print(proc.stdout.decode('utf-8'))
-
-    def testos(self):
-        print(self.output_dir)
-        print(os.getcwd())
 
 ptau = PTau()
 ptau.start()
