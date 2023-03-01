31f5796ca2b658dd1d7dfdc40df9c66e29e449ae
sahilmahendrakar <sahilm020@gmail.com>
2023-03-01 15:57:56 -0500

    Added unit tests for circuit.py

---
+++ zkpy/tests/test_circuit.py
@@ -0,0 +1,64 @@
+from zkpy.circuit import Circuit
+from distutils import dir_util
+from pytest import fixture
+import os
+from pathlib import Path
+
+# example_circuit = """pragma circom 2.0.0;
+
+# /*This circuit template checks that c is the multiplication of a and b.*/  
+
+# template Multiplier2 () {  
+
+#    // Declaration of signals.  
+#    signal input a;  
+#    signal input b;  
+#    signal output c;  
+
+#    // Constraints.  
+#    c <== a * b;  
+# }
+
+# component main = Multiplier2();"""
+
+# example_input = """{"a": "3", "b": "11"}"""
+
+# Uses existing test files for better unit test isolation
+@fixture
+def datadir(tmpdir, request):
+    filename = request.module.__file__
+    test_dir, _ = os.path.splitext(filename)
+
+    if os.path.isdir(test_dir):
+        dir_util.copy_tree(test_dir, str(tmpdir))
+    return Path(tmpdir)
+
+def test_compile_creates_3_files(tmp_path, datadir):
+    circ_file = datadir / 'example_circuit.circom'
+    circ = Circuit(circ_file, working_dir=tmp_path)
+    circ.compile()
+    r1cs_file = tmp_path / "example_circuit.r1cs"
+    sym_file = tmp_path / "example_circuit.sym"
+    js_dir = tmp_path / "example_circuit_js"
+    assert r1cs_file.exists()
+    assert sym_file.exists()
+    assert js_dir.exists()
+
+def test_gen_witness(tmp_path, datadir):
+    circ_file = datadir / 'example_circuit.circom'
+    r1cs_file = datadir / "example_circuit.r1cs"
+    sym_file = datadir / "example_circuit.r1cs"
+    js_dir = datadir / "example_circuit_js"
+    wasm_file = datadir / "example_circuit_js/example_circuit.wasm"
+    circ = Circuit(
+        circ_file, 
+        r1cs=r1cs_file,
+        sym_file=sym_file,
+        js_dir=js_dir,
+        wasm= wasm_file,
+        working_dir=tmp_path)
+    inp_file = datadir / 'input.json'
+    circ.gen_witness(input_file=inp_file)
+    wtns_file = tmp_path / "witness.wtns"
+    assert wtns_file.exists()
+    
