3b36669e37384da49d9d69732f567f89516643ea
sahilmahendrakar <sahilm020@gmail.com>
2023-03-01 15:59:00 -0500

    Cleaned up commented code

--- zkpy/tests/test_circuit.py
+++ zkpy/tests/test_circuit.py
@@ -4,25 +4,6 @@ from pytest import fixture
 import os
 from pathlib import Path
 
-# example_circuit = """pragma circom 2.0.0;
-
-# /*This circuit template checks that c is the multiplication of a and b.*/  
-
-# template Multiplier2 () {  
-
-#    // Declaration of signals.  
-#    signal input a;  
-#    signal input b;  
-#    signal output c;  
-
-#    // Constraints.  
-#    c <== a * b;  
-# }
-
-# component main = Multiplier2();"""
-
-# example_input = """{"a": "3", "b": "11"}"""
-
 # Uses existing test files for better unit test isolation
 @fixture
 def datadir(tmpdir, request):
